42a468bc5ae23e4a8bcea5523753cc91269596fe
Roger Montes <rogerxmontes@gmail.com>
2025-04-15 14:05:56 -0600

    Se agregan rutas usando React Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import { Outlet } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <main className="text-lg text-red-400 font-bold">React App</main>;
+  return (
+    <main>
+      <Outlet />
+    </main>
+  );
 }
 
 export default App;
